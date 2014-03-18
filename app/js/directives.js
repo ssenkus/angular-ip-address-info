7547538164726b0f4280a023ce986353076c07e7
Steven Senkus <steven.senkus@yahoo.com>
2014-03-18 03:17:16 -0700

    Map with collection points, I likes it!

--- app/js/directives.js
+++ app/js/directives.js
@@ -99,7 +99,7 @@ angular.module('IpLocatorApp.directives', ['d3'])
     }])
 
 // Placeholder for now!
-    .directive('d3WorldMap', ['$window', '$timeout', 'd3Service', function($window, $timeout, d3Service) {
+    .directive('d3Worldmap', ['$window', '$timeout', 'd3Service', function($window, $timeout, d3Service) {
         return {
             restrict: 'A',
             scope: {
@@ -110,60 +110,26 @@ angular.module('IpLocatorApp.directives', ['d3'])
             link: function(scope, ele, attrs) {
                 d3Service.d3().then(function(d3) {
 
-                    var width = 960,
-                        height = 500;
-
-                    var projection = d3.geo.equirectangular();
-                    var color = d3.scale.category20c();
-//                graticule = d3.geo.graticule();
-
-                    var path = d3.geo.path()
-                        .projection(projection);
-
-                    var svg = d3.select("body").append("svg")
-                        .attr("width", width)
-                        .attr("height", height);
-
-                    d3.json("readme-world.json", function(error, world) {
-                        var countries = topojson.feature(world, world.objects.countries).features,
-                            neighbors = topojson.neighbors(world.objects.countries.geometries);
-
-                        svg.selectAll(".country")
-                            .data(countries)
-                            .enter().insert("path", ".graticule")
-                            .attr("class", "country")
-                            .attr("d", path)
-                            .style("fill", function(d, i) {
-                            return '#000';
-
-                        });
-
-                        svg.append("circle").attr("r", 2).attr("transform", function() {
-                            return "translate(" + projection([-75, 43]) + ")";
-                        }).attr('fill', '#ff0');
-
-                    });
-
-
-
-
-
-
-
-
-
-
-
-
-
                     var renderTimeout;
                     var margin = parseInt(attrs.margin) || 20,
                         barHeight = parseInt(attrs.barHeight) || 20,
                         barPadding = parseInt(attrs.barPadding) || 5;
 
-                    var svg = d3.select(ele[0])
-                        .append('svg')
-                        .style('width', '100%');
+
+                    var width = '100%',
+                        height = 500;
+
+                    var projection = d3.geo.equirectangular();
+                    var color = d3.scale.category20c();
+                    var path = d3.geo.path()
+                        .projection(projection);
+
+                    var svg = d3.select(ele[0]).append("svg")
+                        .attr("width", width)
+                        .attr("height", height);
+
+
+
 
                     $window.onresize = function() {
                         scope.$apply();
@@ -184,53 +150,32 @@ angular.module('IpLocatorApp.directives', ['d3'])
 
                         if (!data)
                             return;
+                        console.log('data',data)
+
                         if (renderTimeout)
                             clearTimeout(renderTimeout);
 
                         renderTimeout = $timeout(function() {
-                            var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                                height = scope.data.length * (barHeight + barPadding),
-                                color = d3.scale.category20(),
-                                xScale = d3.scale.linear()
-                                .domain([0, d3.max(data, function(d) {
-                                    return d.score;
-                                })])
-                                .range([0, width]);
+                            d3.json("http://localhost/github/angular-ip-address-info/app/lib/data/readme-world.json", function(error, world) {
+                                var countries = topojson.feature(world, world.objects.countries).features,
+                                    neighbors = topojson.neighbors(world.objects.countries.geometries);
 
-                            svg.attr('height', height);
+                                svg.selectAll(".country")
+                                    .data(countries)
+                                    .enter().insert("path", ".graticule")
+                                    .attr("class", "country")
+                                    .attr("d", path)
+                                    .style("fill", function(d, i) {
+                                    return '#000';
 
-                            svg.selectAll('rect')
-                                .data(data)
-                                .enter()
-                                .append('rect')
-                                .on('click', function(d, i) {
-                                return scope.onClick({item: d});
-                            })
-                                .attr('height', barHeight)
-                                .attr('width', 140)
-                                .attr('x', Math.round(margin / 2))
-                                .attr('y', function(d, i) {
-                                return i * (barHeight + barPadding);
-                            })
-                                .attr('fill', function(d) {
-                                return color(d.score);
-                            })
-                                .transition()
-                                .duration(1000)
-                                .attr('width', function(d) {
-                                return xScale(d.score);
-                            });
-                            svg.selectAll('text')
-                                .data(data)
-                                .enter()
-                                .append('text')
-                                .attr('fill', '#fff')
-                                .attr('y', function(d, i) {
-                                return i * (barHeight + barPadding) + 15;
-                            })
-                                .attr('x', 15)
-                                .text(function(d) {
-                                return d.name + " (scored: " + d.score + ")";
+                                });
+
+                                svg.selectAll('circle').data(data).enter().append('circle').attr("r", 2).attr("transform", function(d, i) {
+                                    console.log(d, i)
+                                    //return "translate(" + projection([-75, 43]) + ")";
+                                    return "translate(" + projection([d.longitude, d.latitude]) + ")";
+                                }).attr('fill', '#f00');
+
                             });
                         }, 200);
                     };

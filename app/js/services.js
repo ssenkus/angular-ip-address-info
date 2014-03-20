5ca7602076dcc51bb43aa3b22bff6ccebbcbe995
Steve <steven.senkus@yahoo.com>
2014-03-19 21:35:41 -0700

    Revert "Revert "Moved code from controller to locationHandler service, added JSON files, changed views""
    This reverts commit 700263e746fd36949de1da961828dcf820297843.

--- app/js/services.js
+++ app/js/services.js
@@ -1,25 +1,57 @@
 'use strict';
 
 /* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('IpLocatorApp.services', []).
-    service('locationHandler', function() {
+    service('locationHandler', function($http) {
+    var that = this;
     this.locations = [];
     this.addLocation = function(location) {
         console.log('location', location);
-        
+
         if (location.city === "") {
-           location.locStatus = "warn" 
+            location.locStatus = "warn";
         } else {
-            location.locStatus = "ok"
-            
+            location.locStatus = "ok";
         }
-        
+
         this.locations.push(location);
         console.log('added location', this.locations);
+    };
+    this.deleteLocation = function(idx) {
+        this.locations.splice(idx, 1);
+    };    
+    
+    this.addedTestVals = false;
+    this.addValidIps = function() {
+        if (this.addedTestVals) {
+            return;
+        } else {
+            var newer = [
+                '52.43.90.82',
+                '14.21.124.55',
+                '22.54.76.202',
+                '24.4.76.202',
+                '24.24.24.24',
+                '84.45.22.12'
+            ];
+            newer.forEach(this.getIp);
+            this.addedTestVals = true;
+            return;
+        }
+    };
+    this.getIp = function(ip) {
+        $http({
+            method: 'GET',
+            url: 'http://www.freegeoip.net/json/' + ip
+        }).
+            success(function(data, status, headers, config) {
+            console.log('success', data);
+            that.addLocation(data);
+        }).
+            error(function(data, status, headers, config) {
+            console.log('error', status);
+        });
+
     };
 
 

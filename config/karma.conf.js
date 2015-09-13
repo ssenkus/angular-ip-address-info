375a90baee2d0570e05ca4de6157c59d2dfe8dc7
Steven Senkus <steve@Stevens-MacBook-Pro.local>
2015-09-12 22:24:28 -0700

    cleanup, new unit tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,27 +1,31 @@
-module.exports = function(config){
+module.exports = function (config) {
     config.set({
-    basePath : '../',
-
-    files : [
-      'app/vendor/angular/angular.js',
-      'test/lib/angular/angular-mocks.js',
-      'app/js/modules/*.js',
-      'app/js/controllers/*.js',
-      'test/unit/**/*.js'
-    ],
-
-    exclude : [],
-
-    autoWatch : true,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['Chrome'],
-
-    plugins : [
+        basePath: '../',
+        files: [
+            // Project dependencies
+            'app/vendor/angular/angular.js',
+            'app/vendor/angular-bootstrap/ui-bootstrap.js',
+            'app/vendor/angular-ui-router/release/angular-ui-router.js',
+            'app/vendor/angular-spinner/angular-spinner.js',
+            'app/vendor/underscore/underscore.js',
+            // App files
+            'app/js/modules/*.js',
+            'app/js/controllers/*.js',
+            'app/js/services/*.js',
+            // Test dependencies
+            'test/lib/angular/angular-mocks.js',
+            // Test files
+            'test/unit/**/*.js'
+        ],
+        exclude: [],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['Chrome'],
+        plugins: [
             'karma-junit-reporter',
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine'
-            ],
-})}
+        ]
+    });
+};
